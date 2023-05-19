d3ed74efe88f48926200dce344889c3e7af79dd4
Leewan <959844936@qq.com>
2023-05-19 11:11:48 +0800

    fix: 修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/workshop-CN/' : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
